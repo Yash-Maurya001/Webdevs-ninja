b92681d6bbea87ea9d8236f6f6da7e7db656b2c8
Yash Maurya <yashmaurya.in@gmail.com>
2023-12-17 20:24:24 +0530

    Add counter functionality to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-
-
+import { useState } from "react";
 
 function App() {
-
+  const [count, setCount] = useState(0);
+  const set_count = () => {
+    setCount(count + 1);
+  };
 
   return (
     <>
-      <h1 className="">Hello World</h1>
+      <div className="w-full h-screen flex justify-center items-center mt-[-100px] flex-col gap-6">
+        <h1 className="text-3xl font-bold">Counter</h1>
+        <div className=" flex  gap-4 items-center ">
+          {count}
+          <button className="bg-blue-400 px-3 py-2 rounded" onClick={set_count}>
+            UPDATE
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
